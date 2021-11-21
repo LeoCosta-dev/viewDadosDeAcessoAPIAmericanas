056253cdea6cb7cf9f22dc4bfbc30ddcc541796d
LeoCosta-dev <leonardodacostadasilva@hotmail.com>
2021-11-21 01:22:26 -0300

    motor de busca inserido com sucesso

--- models/model.js
+++ models/model.js
@@ -3,7 +3,7 @@ function busca(str){
         url: `https://mystique-v2-americanas.juno.b2w.io/autocomplete?content=${str}&source=nanook`,
         method: 'get',
         success: (response) => {
-            console.log(response)           
+            insereCards(response.products)           
         }
     })
     }
--- view/view.js
+++ view/view.js
@@ -3,6 +3,7 @@ const buscarHeader = document.getElementById("buscar-header")
 const pesquisaSection = document.getElementById("pesquisa-section");
 const buscarSection = document.getElementById("buscar-section")
 const botaoBusca = document.getElementsByClassName("buscar");
+const cards = document.getElementById("cards");
 
 function campoDeBuscaSection(){
     pesquisaHeader.style.display = "flex";
@@ -12,4 +13,34 @@ function campoDeBuscaSection(){
 
 function campoDeBuscaHeader(){
     busca(buscarHeader.value)
+}
+
+function insereCards(arr){
+    cards.innerHTML = ""
+    for(let i = 0; i < arr.length; i++){
+        cards.innerHTML+=`<div class = "card-produtos">
+        <div class = "nome"> 
+            <h3> ${arr[i].name} </h3> 
+        </div>
+        <div class = "id">
+            <p>Id</p>
+            <p> ${arr[i].id} </p>
+        </div>
+        <div class = "tipo">
+            <p>Tipo</p>
+            <p> ${arr[i].type} </p>
+        </div>
+        <div class = "meta">
+            <div class = "pontos">
+                <p>Pontuação</p>
+                <p> ${arr[i]._meta.score} </p>
+            </div>
+            <div class = "visitas">
+                <p>visitas no site</p>
+                <p> ${arr[i]._meta.visitsClickCount} </p>
+            </div>
+        </div>
+
+    </div>`
+    }
 }
